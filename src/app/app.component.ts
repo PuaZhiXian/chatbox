170ab6768aeb5279f4d18acc944175f944acee49
PuaZhiXian <xianpua2001@gmail.com>
2023-12-12 18:37:47 +0800

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component, ElementRef, ViewChild} from '@angular/core';
+import {IMessage} from "./interface/i-message";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,62 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chatbox';
+  text: string = '';
+
+  messageHistory: IMessage[] = []
+  @ViewChild('textbox') content!: ElementRef;
+
+  constructor(private ref: ChangeDetectorRef,) {
+  }
+
+  chat() {
+    if (this.text.trim().length > 0) {
+      this.messageHistory.push({
+        id: this.messageHistory.length,
+        user: 'You',
+        message: this.text.trim(),
+        date: new Date(),
+      })
+
+      let reply: string = '';
+      let lowerCaseText = this.text.trim().toLowerCase();
+      if (this.text === this.text.toUpperCase()) {
+        reply = 'Please remain calm.';
+        if ((lowerCaseText.includes(' how ') || lowerCaseText.includes(' what ') || lowerCaseText.includes(' why ') || lowerCaseText.includes(' who ') || lowerCaseText.includes(' which ') || lowerCaseText.includes(' ?'))) {
+          reply = 'Please give me some time to resolve the issue.';
+        }
+      } else {
+        if (lowerCaseText.includes(' how ') || lowerCaseText.includes(' what ') || lowerCaseText.includes(' why ') || lowerCaseText.includes(' who ') || lowerCaseText.includes(' which ') || lowerCaseText.includes(' ?')) {
+          reply = 'Yes';
+        } else if (lowerCaseText == 'jamie') {
+          reply = 'Can I help you?';
+        } else {
+          reply = 'Sorry, I don’t understand';
+        }
+      }
+      this.messageHistory.push({
+        id: this.messageHistory.length,
+        user: 'Jamie',
+        message: reply,
+        date: new Date(),
+      })
+      this.text = '';
+      this.ref.detectChanges();
+      this.ref.markForCheck();
+      this.scrollToSection('bottom');
+    } else {
+      this.text = ''
+    }
+
+  }
+
+  scrollToSection(sectionId: string) {
+    const element = this.content.nativeElement.querySelector(`#${sectionId}`);
+    console.log(sectionId)
+    if (element) {
+      element.scrollIntoView({behavior: 'smooth'});
+    }
+  }
+
+
 }
---
+++ src/app/interface/i-message.ts
@@ -0,0 +1,6 @@
+export interface IMessage {
+  id: number;
+  user: string;
+  message: string;
+  date: Date;
+}
